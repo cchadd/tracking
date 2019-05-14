e6f158070daf629f1ea508627bce321220bc41a2
cchadd <clement.chadebec@mines-paristech.fr>
2019-05-14 22:35:24 +0200

    minor fixes

--- calibration/mean_selection_calibration.py
+++ calibration/mean_selection_calibration.py
@@ -1,6 +1,7 @@
-import numpy as np
+import numpy as np 
 import cv2
 from calibration.framesProcess import FramesProcess
+
 
 
 class MeanSelectionCalib(FramesProcess):
@@ -18,21 +19,21 @@ class MeanSelectionCalib(FramesProcess):
 
         path_to_frames (str)
             Path to the folder in which frames will be stored for further process
-
+        
         real_coordinates (dict)
             Dictionnary with real coordinate {0: (x0, y0),
                                               1, (x1, y1),
                                               ...}
-            By convention:
+            By convention: 
                 - selected points for calibration will have a positive key
                 - selected points for testing calibration will have a negative key
 
         name_to_frame (str)
             Name to be giver to the frames name_to_frames_Number_Of_frame
-
+        
         num_frames (int)
             Number of frames on which perform the calibration
-
+        
         frame_delay (int, float)
             Delay between to recorded frames
         '''
@@ -40,7 +41,7 @@ class MeanSelectionCalib(FramesProcess):
         assert isinstance(real_coordinates, dict)
         assert isinstance(num_frames, int)
         assert isinstance(frame_delay, (int, float))
-
+        
 
         FramesProcess.__init__(self)
         self.__path_to_video = path_to_video
@@ -53,11 +54,31 @@ class MeanSelectionCalib(FramesProcess):
         self.__real_coordinates = []
         self.__testing_coordinates = []
 
+        #Vector points
+        self.__image_p_vec = None
+        self.__real_p_vec = None
+        
         #Calibration properties
         self.__camera_matrix = None
-        self.__distorsion = None
+        self.__distorsion = None 
         self.__rot_matrix = None
         self.__tran_matrix = None
+        
+    @property
+    def image_p_vec(self):
+        return self.__image_p_vec
+
+    @property
+    def real_p_vec(self):
+        return self.__real_p_vec
+
+    @property
+    def rot_matrix(self):
+        return self.__rot_matrix
+
+    @property
+    def tran_matrix(self):
+        return self.__tran_matrix
 
     @property
     def camera_matrix(self):
@@ -66,7 +87,7 @@ class MeanSelectionCalib(FramesProcess):
     @property
     def distortion(self):
         return self.__distorsion
-
+        
     def calibrate_camera(self):
         self.get_frames(
                     self.__path_to_video,
@@ -81,7 +102,7 @@ class MeanSelectionCalib(FramesProcess):
 
         sample_frame = self.get_a_frame(
             self.__path_to_frames, 0)
-
+        
         self.__compute_mean_array(image_coordinate)
         self.__get_object_point()
 
@@ -91,16 +112,16 @@ class MeanSelectionCalib(FramesProcess):
         except:
             print('Dimension of image and object points do not match')
 
-        image_coord_shaped, real_coord_shaped = self.__reshaping()
+        self.__reshaping()
 
         camera_matrix = cv2.initCameraMatrix2D(
-            [real_coord_shaped],
-            [image_coord_shaped],
+            [self.real_p_vec],
+            [self.image_p_vec],
             sample_frame.shape[:2])
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-            [real_coord_shaped],
-            [image_coord_shaped],
+            [self.real_p_vec],
+            [self.image_p_vec],
             sample_frame.shape[:2],
             camera_matrix,
             None,
@@ -133,17 +154,17 @@ class MeanSelectionCalib(FramesProcess):
                 mean[1] = mean[1]/len(image_coordinate_list)
                 points_mean.append(mean)
             self.__image_coordinates = np.array(list(points_mean))
-
+        
         except IndexError:
             raise IndexError('Number of coord per frame do not match')
 
-
+    
     def __get_object_point(self):
         '''
-        Stored selected coordinates (key >= 0)
+        Stored selected coordinates (key >= 0) 
         order = increasing key's value
         '''
-
+        
         selected_keys = [
             key for key in sorted(
                 self.__real_coordinates_dict.keys()) if key >= 0]
@@ -153,10 +174,10 @@ class MeanSelectionCalib(FramesProcess):
         self.__real_coordinates = np.array(
             self.__real_coordinates)
 
-    def __reshaping(self ):
+    def __reshaping(self):
         ''' Reshape image and real coordinates to perform the calibration
 
-        Oustputs:
+        Outputs:
         --------
         reshaped_array (array)
             Reshaped array
@@ -179,4 +200,5 @@ class MeanSelectionCalib(FramesProcess):
         print (image_p_vec, real_p_vec)
         image_p_vec = image_p_vec.reshape(1, -1, 2).astype('float32')
 
-        return image_p_vec, real_p_vec
+        self.__image_p_vec = image_p_vec
+        self.__real_p_vec = real_p_vec    
