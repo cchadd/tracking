2e217da80f4e2cf1e65fe97e7ef966610e81cfab
cchadd <clement.chadebec@mines-paristech.fr>
2019-06-03 22:54:11 +0200

    team class

---
+++ stats/team.py
@@ -0,0 +1,35 @@
+#%%
+from stats.player import Player
+import yaml
+
+class Team(object):
+    
+    def __init__(self, team_name, composition, color):
+        assert isinstance(team_name, str)
+        assert isinstance(composition, dict)
+        assert isinstance(color, str)
+
+        self.composition = dict.fromkeys(composition)
+
+        for player in self.composition.keys():
+            player_param = composition[player]
+            self.composition[player] = Player(team=team_name, **player_param)
+
+                
+
+
+class Game(object):
+
+    def __init__(self, yaml_file):
+        with open(yaml_file) as ffile:
+            params = yaml.load(ffile)
+
+        team_1 = params['team_A']
+        
+
+        self.team = Team(**team_1)
+
+#%%
+game = Game('./stats/config/sample_game_config.yaml')
+
+#%%
